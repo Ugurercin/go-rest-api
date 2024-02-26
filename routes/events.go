388ac891594385fdc2a013f48de65b07ce13168c
Uğur Erçin <ugur@Ugurs-MacBook-Air.local>
2024-02-26 20:14:47 +0300

    route and event logic outsourced to routes package

---
+++ routes/events.go
@@ -0,0 +1,60 @@
+package routes
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"example.com/rest-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func getEvents(ctx *gin.Context) {
+	events, err := models.GetAllEvents()
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, events)
+
+}
+
+func getEventById(ctx *gin.Context) {
+	id, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid event ID"})
+		return
+	}
+
+	event, err := models.GetEventById(id)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, event)
+}
+
+func createEvent(ctx *gin.Context) {
+	var event models.Event
+
+	err := ctx.ShouldBindJSON(&event)
+
+	if err != nil {
+		fmt.Println("Error binding JSON:", err)
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Could not parsed request data."})
+		return
+	}
+
+	event.ID = 1
+	event.UserId = 1
+
+	err = event.Save()
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save event."})
+
+	}
+	ctx.JSON(http.StatusCreated, gin.H{"message": "Event created !", "event": event})
+}
