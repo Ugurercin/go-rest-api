2efd86efcf92b8227cf5b1a7058fd26b2b1f5659
Uğur Erçin <ugur@Ugurs-MacBook-Air.local>
2024-02-26 20:09:09 +0300

    create,get,getbyid endpoints added

---
+++ models/event.go
@@ -0,0 +1,72 @@
+package models
+
+import (
+	"fmt"
+	"time"
+
+	"example.com/rest-api/db"
+)
+
+type Event struct {
+	ID          int64
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
+	DateTime    time.Time `binding:"required"`
+	UserId      int
+}
+
+var events = []Event{}
+
+func (e *Event) Save() error {
+	query := `INSERT INTO events(name, description, location, dateTime, user_id) VALUES (?, ?, ?, ?, ?)`
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserId)
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	if err == nil {
+		e.ID = id // Only update ID if LastInsertId() succeeds
+	}
+	return err
+}
+
+func GetEventById(id int64) (*Event, error) {
+	query := "SELECT * FROM events WHERE id = ?"
+	row := db.DB.QueryRow(query, id)
+
+	fmt.Print(row)
+	var e Event
+	err := row.Scan(&e.ID, &e.UserId, &e.Name, &e.Description, &e.Location, &e.DateTime)
+	if err != nil {
+		return nil, err
+	}
+	return &e, nil
+}
+
+func GetAllEvents() ([]Event, error) {
+	query := "SELECT * FROM events"
+	rows, err := db.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var events []Event
+	for rows.Next() {
+		var e Event
+		err := rows.Scan(&e.ID, &e.UserId, &e.Name, &e.Description, &e.Location, &e.DateTime)
+		if err != nil {
+			return nil, err
+		}
+		events = append(events, e)
+	}
+	return events, nil
+}
