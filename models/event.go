9ef8fc060f238e5ffde7e67d5c9a8a753a580ae8
Uğur Erçin <ugur@Ugurs-MacBook-Air.local>
2024-02-26 20:54:17 +0300

    update&delete operations and api test added

--- models/event.go
+++ models/event.go
@@ -70,3 +70,33 @@ func GetAllEvents() ([]Event, error) {
 	}
 	return events, nil
 }
+
+func (e *Event) Update() error {
+	query := `UPDATE events 
+	SET name = ?, description = ?, location = ?, dateTime = ? 
+	WHERE id = ?`
+
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(e.ID, e.Name, e.Description, e.Location, e.DateTime)
+	return err
+}
+
+func (e Event) Delete() error {
+	query := "DELETE FROM events WHERE id = ?"
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(e.ID)
+	return err
+
+}
--- routes/events.go
+++ routes/events.go
@@ -58,3 +58,63 @@ func createEvent(ctx *gin.Context) {
 	}
 	ctx.JSON(http.StatusCreated, gin.H{"message": "Event created !", "event": event})
 }
+
+func updateEvent(ctx *gin.Context) {
+	eventId, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid event ID"})
+		return
+	}
+
+	_, err = models.GetEventById(eventId)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	var updatedEvent models.Event
+
+	err = ctx.ShouldBindJSON(&updatedEvent)
+
+	if err != nil {
+		fmt.Println("Error binding JSON:", err)
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Could not parsed request data."})
+		return
+	}
+	updatedEvent.ID = eventId
+
+	err = updatedEvent.Update()
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not update event."})
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "Event updated !", "event": updatedEvent})
+
+}
+
+func deleteEvent(ctx *gin.Context) {
+	eventId, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid event ID"})
+		return
+	}
+
+	e, err := models.GetEventById(eventId)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	err = e.Delete()
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not delete event."})
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"message": "Event deleted !"})
+
+}
